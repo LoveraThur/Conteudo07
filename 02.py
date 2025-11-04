80ebbbe4d7e5103e99cdf21d252e7f200d64508a
Arthur Lovera <arthuralbaralovera@gmail.com>
2025-11-04 20:29:59 -0300

    ex02

--- 02.py
+++ 02.py
@@ -1,4 +1,24 @@
-UF = {61:'Brasilia',
-      71:'Salvador' }
+UF = [{'DDD': 61, 'Destino': 'Brasilia'},
+      {'DDD': 71, 'Destino': 'Salvador'},
+      {'DDD': 11, 'Destino': 'São paulo'},
+      {'DDD': 21, 'Destino': 'Rio de Janeiro'},
+      {'DDD': 32, 'Destino': 'Juiz de Fora'}, 
+      {'DDD': 19, 'Destino': 'Campinas'},
+      {'DDD': 27, 'Destino': 'Vitória'},
+      {'DDD': 31, 'Destino': 'Belo Horizonte'}
+]
+while True:
+      try:
+            number = int(input('Digite o DDD desejado \n>>> '))
+      except:
+            print('\033[31mDigite um número válido\033[m')
+            print('-'*30)
+      else:
+            break
 
-print(UF)
+for e in UF:
+      if number in e.values():
+            print(f'O DDD {number} é de \033[34m{e['Destino']}\033[m')
+            break
+else:
+      print('\033[31mDDD não cadastrado!\033[m')
